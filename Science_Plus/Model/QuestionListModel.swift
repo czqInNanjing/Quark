d693e61688c6e977b2f711d4a36a3cf98bcfa867
czqInNanjing <czqofnju@gmail.com>
2016-07-20 21:27:01 +0800

    Development: add simple notice

--- Science_Plus/Model/QuestionListModel.swift
+++ Science_Plus/Model/QuestionListModel.swift
@@ -12,7 +12,7 @@ class QuestionListModel {
     
     private var currentPage = 1
     private var questions = [Array<Question>]()
-    func getQuestionsByPage(questionTable qtable:QuestionViewController? )  {
+    func getQuestionsByPage(isRefresh:Bool = false , questionTable qtable:QuestionViewController? )  {
         
         
         
@@ -28,15 +28,32 @@ class QuestionListModel {
                     self?.questions.append(newQuestions)
                     qtable?.table.reloadData()
                     self?.currentPage += 1
+                    if isRefresh{
+                        qtable?.noticeSuccess("刷新成功")
+                    }else{
+//                        qtable?.noticeSuccess("加载成功")
+                    }
+                    
+                }else{
+                    if isRefresh{
+                        
+                    } else{
+                        qtable?.noticeInfo("没有更多了")
+                    }
                 }
             }else{
                 print("Question ErrorReason: \(json)")
                 //handler(json)
-                
+                qtable?.noticeError("网络好像出了点问题")
             }
             // stop the indicator animation
-            qtable?.table.finishInfiniteScroll()
-            qtable?.table.dg_stopLoading()
+            if isRefresh{
+                qtable?.table.dg_stopLoading()
+            }else{
+                qtable?.table.finishInfiniteScroll()
+
+            }
+            
             
         }
         print("current question page:\(currentPage)")
@@ -48,7 +65,7 @@ class QuestionListModel {
     func refreshData(questionTable qtable:QuestionViewController?) {
         questions.removeAll()
         currentPage = 1
-        getQuestionsByPage(questionTable: qtable)
+        getQuestionsByPage(true, questionTable: qtable)
         
         
     }
